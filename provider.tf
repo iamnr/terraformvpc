8427c25d290421f92dbef067e1dfc150fd18b2b2
iamnr <avuthunaveenreddy@gmail.com>
2024-10-17 08:11:39 +0530

    terraform

--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket = "terraformstatefiletest"
-    key    = "terraform vpc"
+    key    = "terraform vpc test"
     region = "us-east-1"
     dynamodb_table = "roboshoplocking"  
     }
